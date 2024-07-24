eae836e2577284c99eddc3748b93e4afce05f39a
Krzysztof Perdek <krzysztofperdek@gmail.com>
2024-07-24 20:35:50 +0200

    sddm fix + hyprland changes

--- .config/hypr/scripts/changewallpaper.sh
+++ .config/hypr/scripts/changewallpaper.sh
@@ -4,9 +4,10 @@ if ! [[ -z "$1" ]]; then
 	if [ -f "$1" ]; then
 		hyprctl hyprpaper unload "$HOME/Wallpapers/current.png" >/dev/null
 		magick "$1" "$HOME/Wallpapers/current.png" >/dev/null
+		cp "$HOME/Wallpapers/current.png" /usr/share/sddm/themes/sddm-astronaut-theme/Wallpaper.png >/dev/null
 		hyprctl hyprpaper preload "$HOME/Wallpapers/current.png" >/dev/null &&
 			hyprctl hyprpaper wallpaper "eDP-1,$HOME/Wallpapers/current.png" >/dev/null &&
-			wal -i "$1" --saturate 0.7 >/dev/null &&
+			wal -i "$1" -e -n --saturate 0.7 >/dev/null &&
 			eww reload >/dev/null
 	fi
 fi
